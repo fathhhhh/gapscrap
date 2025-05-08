c5c0228278b09398524fa80887b03adcb7142bc9
fathurrahman <fath@mesen.local>
2025-05-08 11:17:37 +0800

    add +6 address

--- routes/ws.js
+++ routes/ws.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/:input', (req, res) => {
     const userInput = req.params.input
-    const redirectUrl = `https://api.whatsapp.com/send?phone=${userInput}`
+    const redirectUrl = `https://api.whatsapp.com/send?phone=6${userInput}`
     res.redirect(301, redirectUrl);
 });
 
